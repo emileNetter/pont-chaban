aeeecda827f468b9c7d2c2c5f9d9702c91eaefd7
Emile Netter <emile@poool.fr>
2024-05-22 09:58:31 +0200

    fix: update resources on every request

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import NextClosingDate from "./NextClosingDate";
 
 const Home = async () => {
   const dataUrl = 'https://opendata.bordeaux-metropole.fr/api/explore/v2.1/catalog/datasets/previsions_pont_chaban/records?where=date_passage%20%3E%20now()&limit=20';
-  const data = await fetch(dataUrl).then(res => res.json()) as Data;
+  const data = await fetch(dataUrl, { cache: 'no-store' })
+    .then(res => res.json()) as Data;
 
   return (
     <main className="container min-h-screen mx-auto px-4 bg-image">
